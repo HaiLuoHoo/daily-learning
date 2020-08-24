3823aba566142ed5be743d86c968096839a45b2d
HaiLuoHoo <70054354+HaiLuoHoo@users.noreply.github.com>
2020-08-24 17:48:14 +0800

    Add files via upload

---
+++ 单链表.cpp
@@ -0,0 +1,130 @@
+using namespace std;
+#include<iostream>
+#define endl '\n'
+//实现链表的遍历插入和删除操作 
+struct Node{
+	int data;
+	Node *next;
+};
+class LinkList{
+	private:
+		Node *head;
+	public:
+		LinkList(){};
+		~LinkList();
+		void CreateList1(int n);//头插法 
+		void CreateList2(int n);//尾插法 
+		void ListInsert(int i,int e);//插入元素e到第i个位置 
+		void ListDelete(int n);//删除第n个元素 
+		int GetElem(int i);//获取第i个元素 
+		void GetList();
+		int ListLength();//获取链表的长度 
+};
+void LinkList::CreateList1(int n){
+	Node *p,*q;
+	head=0;
+	cout<<"请依次输入"<<n<<"个数据元素值:"<<endl;
+	for(int i=0;i<n;i++){
+		p=head;
+		q=new Node;
+		cin>>q->data;
+		head=q;
+		q->next=p;
+	}
+}
+void LinkList::CreateList2(int n){
+	Node *p,*q;
+	p=head;
+	cout<<"请依次输入"<<n<<"个数据元素值:"<<endl;
+	for(int i=0;i<n;i++){
+		q=new Node;
+		cin>>q->data;
+		q->next=0;
+		p->next=q;
+		p=q;
+	}
+}
+void LinkList::ListInsert(int i,int e){
+	Node *p,*q,*r;
+	p=head;
+	if(i==1){
+		r=new Node;
+		r->data=e;
+		r->next=p;
+		head=r; 
+	}
+	else{
+		for(int j=0;j<i-2;j++){
+			p=p->next;
+		}
+		q=p->next;
+		r=new Node;
+		r->data=e;
+		r->next=q;
+		p->next=r;
+	}
+}
+void LinkList::ListDelete(int n){
+	Node *p,*q;
+	p=head;
+	if(n==1){
+		head=p->next;
+		delete p;
+	}
+	else{
+		for(int j=0;j<n-2;j++){
+			p=p->next;
+		}
+		q=p->next;
+		p->next=q->next;
+		delete q;
+	}
+}
+int LinkList::GetElem(int i){
+	Node *p;
+	p=head;
+	for(int j=0;j<i-1;j++){
+		p=p->next;
+	}
+	return p->data;
+}
+void LinkList::GetList(){
+	Node *p;
+	int n;
+	p=head;
+	while(p){
+		cout<<p->data<<' ';
+		p=p->next;
+	}
+	cout<<endl;
+}
+int LinkList::ListLength(){
+	Node *p;
+	int n=0;
+	p=head;
+	while(p){
+		p=p->next;
+		n++;
+	}
+	return n;
+}
+LinkList::~LinkList(){
+	Node *p=0;
+	while(p){
+		p=head->next;
+		delete head;
+		head=p;
+	}
+}
+int main(){
+	LinkList list1;
+	list1.CreateList1(6);
+	cout<<list1.GetElem(4)<<endl;
+	list1.GetList();
+	list1.ListInsert(2,999);
+	list1.GetList();
+	cout<<"链表的长度："<<list1.ListLength()<<endl;
+	list1.ListDelete(2);
+	list1.GetList();
+	return 0;
+}
